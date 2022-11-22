2e0d964a6a1af54581c07f3581a87b3132db7fe4
wael-fadlallah <wael.fudlallah@gmail.com>
2022-11-22 22:31:16 +0400

    configure react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,28 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {SafeAreaView, Text, useColorScheme} from 'react-native';
-
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {SafeAreaView, Text} from 'react-native';
+import {Button} from 'react-native-paper';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView>
       <Text>Hello world</Text>
+      <Button
+        icon={'camera'}
+        mode="contained"
+        onPress={() => console.log('Harder daddy')}>
+        Spank me
+      </Button>
     </SafeAreaView>
   );
 };
--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 /**
  * @format
  */
-
+import * as React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {Provider as PaperProvider} from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => (
+  <PaperProvider>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
